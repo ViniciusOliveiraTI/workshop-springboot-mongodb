98fe17baaa93b66b22ecca2cd803ddb0fc56c07f
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-09-30 12:24:31 -0300

    Obtendo um usuário por id

--- src/main/java/com/viniciusdev/mongo/resources/UserResource.java
+++ src/main/java/com/viniciusdev/mongo/resources/UserResource.java
@@ -5,12 +5,11 @@ import com.viniciusdev.mongo.dto.UserDTO;
 import com.viniciusdev.mongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,9 +25,15 @@ public class UserResource {
         List<User> users = userService.findAll();
         List<UserDTO> usersDto = users
                 .stream()
-                .map(x -> new UserDTO(x))
+                .map(obj -> new UserDTO(obj))
                 .collect(Collectors.toList());
         return ResponseEntity.ok().body(usersDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 
 }
---
+++ src/main/java/com/viniciusdev/mongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.viniciusdev.mongo.resources.exceptions;
+
+import com.viniciusdev.mongo.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        String error = "Não encontrado";
+
+        StandardError err = new StandardError(Instant.now(), status, error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+}
