9be77a55862956273043189ec4581394a52e1eca
unknown <vinicius.oliveira@grupotechnik.com.br>
2025-10-01 07:46:55 -0300

    Consulta simples com query methods

--- src/main/java/com/viniciusdev/mongo/resources/PostResource.java
+++ src/main/java/com/viniciusdev/mongo/resources/PostResource.java
@@ -1,13 +1,11 @@
 package com.viniciusdev.mongo.resources;
 
 import com.viniciusdev.mongo.domain.Post;
+import com.viniciusdev.mongo.resources.util.URL;
 import com.viniciusdev.mongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post object = postService.findById(id);
         return ResponseEntity.ok().body(object);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> posts = postService.findByTitle(text);
+        return ResponseEntity.ok().body(posts);
     }
 
 }
